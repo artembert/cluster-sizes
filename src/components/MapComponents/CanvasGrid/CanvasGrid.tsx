599fb14b670e7d5f783cdee9dd7daf249cd69bc2
Artem Beresnev <artembert@gmail.com>
2022-01-03 21:05:12 +0300

    Convert arrow functions to function expressions

--- src/components/MapComponents/CanvasGrid/CanvasGrid.tsx
+++ src/components/MapComponents/CanvasGrid/CanvasGrid.tsx
@@ -2,6 +2,10 @@ import { FunctionComponent } from "react";
 import { useGridSellSize } from "../../../contexts/GridCellSizeContext";
 import Canvas from "../Canvas/Canvas";
 
+/**
+ * Image: https://eperezcosano.github.io/hex-grid/
+ */
+
 interface Props {
   width: number;
   height: number;
@@ -9,10 +13,10 @@ interface Props {
 
 const HEXAGON_ANGLE = (2 * Math.PI) / 6;
 
-const getYStartCoordsForAllLines: (
+function getYStartCoordsForAllLines(
   height: number,
   hexagonRadius: number
-) => number[] = (height, hexagonRadius) => {
+): number[] {
   const yStartCoords = [];
   let y = hexagonRadius * Math.sin(HEXAGON_ANGLE);
   while (y + 2 * hexagonRadius * Math.sin(HEXAGON_ANGLE) < height) {
@@ -20,50 +24,52 @@ const getYStartCoordsForAllLines: (
     y += 2 * hexagonRadius * Math.sin(HEXAGON_ANGLE);
   }
   return yStartCoords;
-};
+}
+
+function drawHexagon(
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  hexagonRadius: number
+): void {
+  ctx.beginPath();
+  for (var i = 0; i < 6; i++) {
+    ctx.lineTo(
+      x + hexagonRadius * Math.cos(HEXAGON_ANGLE * i),
+      y + hexagonRadius * Math.sin(HEXAGON_ANGLE * i)
+    );
+  }
+  ctx.closePath();
+  ctx.stroke();
+}
+
+function drawGrid(
+  ctx: CanvasRenderingContext2D,
+  width: number,
+  height: number,
+  hexagonRadius: number
+): void {
+  ctx.clearRect(0, 0, width, height);
+  const yStartCoords = getYStartCoordsForAllLines(height, hexagonRadius);
+  yStartCoords.forEach((y) => {
+    let x = hexagonRadius;
+    let j = 0;
+    while (x + hexagonRadius * (1 + Math.cos(HEXAGON_ANGLE)) < width) {
+      drawHexagon(ctx, x, y, hexagonRadius);
+      x += hexagonRadius * (1 + Math.cos(HEXAGON_ANGLE));
+      y += (-1) ** j * hexagonRadius * Math.sin(HEXAGON_ANGLE);
+      j++;
+    }
+  });
+}
 
 const CanvasGrid: FunctionComponent<Props> = ({ width, height }) => {
   const { value: gridSellSize } = useGridSellSize();
   const hexagonRadius = gridSellSize / 2;
-  const yStartCoords = getYStartCoordsForAllLines(height, hexagonRadius);
 
-  const drawHexagon: (
-    ctx: CanvasRenderingContext2D,
-    x: number,
-    y: number
-  ) => void = (ctx, x, y) => {
-    ctx.beginPath();
-    for (var i = 0; i < 6; i++) {
-      ctx.lineTo(
-        x + hexagonRadius * Math.cos(HEXAGON_ANGLE * i),
-        y + hexagonRadius * Math.sin(HEXAGON_ANGLE * i)
-      );
-    }
-    ctx.closePath();
-    ctx.stroke();
-  };
-
-  const drawGrid: (
-    ctx: CanvasRenderingContext2D,
-    width: number,
-    height: number
-  ) => void = (ctx, width, height) => {
-    ctx.clearRect(0, 0, width, height);
-    yStartCoords.forEach((y) => {
-      let x = hexagonRadius;
-      let j = 0;
-      while (x + hexagonRadius * (1 + Math.cos(HEXAGON_ANGLE)) < width) {
-        drawHexagon(ctx, x, y);
-        x += hexagonRadius * (1 + Math.cos(HEXAGON_ANGLE));
-        y += (-1) ** j * hexagonRadius * Math.sin(HEXAGON_ANGLE);
-        j++;
-      }
-    });
-  };
-
-  const draw: (ctx: CanvasRenderingContext2D) => void = (ctx) => {
-    return drawGrid.call(null, ctx, width, height);
-  };
+  function draw(ctx: CanvasRenderingContext2D): void {
+    return drawGrid.call(null, ctx, width, height, hexagonRadius);
+  }
 
   return <Canvas draw={draw} width={width} height={height} />;
 };
