049937a8f22ce7eeec0a047074559f347dc83f6f
Artem Beresnev <artembert@gmail.com>
2022-02-06 14:45:25 +0300

    Fix: Hide hexagon centroids

--- src/components/MapComponents/Map/marker-config.ts
+++ src/components/MapComponents/Map/marker-config.ts
@@ -2,7 +2,7 @@ import { CirclePaint } from "maplibre-gl";
 
 export const clusterStyleConfig: CirclePaint = {
   "circle-color": "transparent",
-  "circle-stroke-color": "red",
+  "circle-stroke-color": "transparent",
   "circle-stroke-width": 1,
   "circle-radius": 28,
 };
